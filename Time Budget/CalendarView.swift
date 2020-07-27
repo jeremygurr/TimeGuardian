10d442f2593b77f6f39042ec59264a0465d74ef2
Jeremy Gurr <jeremy.gurr@gmail.com>
2020-07-27 13:02:02 -0700

    Refactor CalendarView

--- Time Budget/CalendarView.swift
+++ Time Budget/CalendarView.swift
@@ -19,96 +19,113 @@ struct CalendarView: View {
 		_todaysExpenses = TimeExpense.fetchRequestFor(date: Date())
 	}
 
-	func toTimeString(_ period: Int) -> String {
-		let startOfDay = getStartOfDay()
-		let interval = TimeInterval(period * calendarSettings.expensePeriod * 60)
-		let currentPeriod = startOfDay.advanced(by: interval)
-		let timeFormat = DateFormatter()
-		timeFormat.dateFormat = "HH:mm"
-		let timeString = timeFormat.string(from: currentPeriod)
-		return timeString
-	}
-	
-	func getItemIndexOfCurrentTime() -> Int {
-		let now = Date()
-		let startOfDay = getStartOfDay()
-		let difference = Int(startOfDay.distance(to: now))
-		let itemIndex = difference / 60 / calendarSettings.expensePeriod
-		return itemIndex
-	}
-	
-	func colorOfRow(_ row: Int) -> some View {
-		if(row == getItemIndexOfCurrentTime()) {
-			return Color.init("HighlightBackground")
-		}
-		return Color.white
-	}
-	
-	func getExpenseFor(_ period: Int) -> TimeExpense? {
-		var result: TimeExpense? = nil
-		for expense in todaysExpenses {
-			if expense.timeSlot == period {
-				result = expense
-			} else if expense.timeSlot > period {
-				break
-			}
-		}
-		return result
-	}
-	
-	func toExpenseString(_ period: Int) -> String {
-		var result = ""
-		if let existingExpense = self.getExpenseFor(period) {
-			result = existingExpense.fund.name
-		}
-		return result
-	}
-	
 	var body: some View {
 		List {
 			ForEach(0 ..< 10) { _ in
 				Text("")
 			}
-			ForEach(0 ..< calendarSettings.periodsPerDay) { period in
-				HStack {
-					Text("\(self.toTimeString(period))")
-					Spacer()
-					Text("\(self.toExpenseString(period))")
-				}
-				.padding()
-				.contentShape(Rectangle())
-				.background(self.colorOfRow(period))
-				.onTapGesture {
-					if let existingExpense = self.getExpenseFor(period) {
-						self.managedObjectContext.delete(existingExpense)
-						saveData(self.managedObjectContext)
-					} else {
-						if let fund = self.budgetStack.lastSelectedFund {
-							let expense = TimeExpense(context: self.managedObjectContext)
-							expense.fund = fund
-							expense.timeSlot = Int16(period)
-							var pathString = ""
-							let space : Character = " "
-							for f in self.budgetStack.getFunds() {
-								if pathString.count > 0 {
-									pathString.append(space)
-								}
-								pathString.append(contentsOf: "\(f.id)")
-							}
-							expense.path = pathString
-							expense.when = getStartOfDay()
-							saveData(self.managedObjectContext)
-						}
-					}
-				}
-			}
+			ExpenseRowView(todaysExpenses: self.todaysExpenses)
 			ForEach(0 ..< 10) { _ in
 				Text("")
 			}
 		}.introspectTableView { tableView in
-			tableView.scrollToRow(at: IndexPath(item: self.getItemIndexOfCurrentTime() + 10, section: 0), at: .middle, animated: false)
+			tableView.scrollToRow(
+				at: IndexPath(
+					item: getItemIndexOfCurrentTime(calendarSettings: self.calendarSettings) + 10, section: 0)
+				, at: .middle
+				, animated: false
+			)
 		}
 	}
+}
+
+struct ExpenseRowView: View {
+	@EnvironmentObject var calendarSettings: CalendarSettings
+	@EnvironmentObject var budgetStack: BudgetStack
+	var todaysExpenses: FetchedResults<TimeExpense>
+	@Environment(\.managedObjectContext) var managedObjectContext
+
+	var body: some View {
+		ForEach(0 ..< calendarSettings.periodsPerDay) { period in
+			HStack {
+				Text("\(toTimeString(period, calendarSettings: self.calendarSettings))")
+				Spacer()
+				Text("\(toExpenseString(period, todaysExpenses: self.todaysExpenses))")
+			}
+			.padding()
+			.contentShape(Rectangle())
+			.background(colorOfRow(period, calendarSettings: self.calendarSettings))
+			.onTapGesture {
+				if let existingExpense = getExpenseFor(period, todaysExpenses: self.todaysExpenses) {
+					self.managedObjectContext.delete(existingExpense)
+					saveData(self.managedObjectContext)
+				} else {
+					if let fund = self.budgetStack.lastSelectedFund {
+						let expense = TimeExpense(context: self.managedObjectContext)
+						expense.fund = fund
+						expense.timeSlot = Int16(period)
+						var pathString = ""
+						let space : Character = " "
+						for f in self.budgetStack.getFunds() {
+							if pathString.count > 0 {
+								pathString.append(space)
+							}
+							pathString.append(contentsOf: "\(f.id)")
+						}
+						expense.path = pathString
+						expense.when = getStartOfDay()
+						saveData(self.managedObjectContext)
+					}
+				}
+			}
+		}
+	}
+}
+
+func getExpenseFor(_ period: Int, todaysExpenses: FetchedResults<TimeExpense>) -> TimeExpense? {
+	var result: TimeExpense? = nil
+
+	for expense in todaysExpenses {
+		if expense.timeSlot == period {
+			result = expense
+		} else if expense.timeSlot > period {
+			break
+		}
+	}
+	return result
+}
+
+func toExpenseString(_ period: Int, todaysExpenses: FetchedResults<TimeExpense>) -> String {
+	var result = ""
+	if let existingExpense = getExpenseFor(period, todaysExpenses: todaysExpenses) {
+		result = existingExpense.fund.name
+	}
+	return result
+}
+
+func colorOfRow(_ row: Int, calendarSettings: CalendarSettings) -> some View {
+	if(row == getItemIndexOfCurrentTime(calendarSettings: calendarSettings)) {
+		return Color.init("HighlightBackground")
+	}
+	return Color.white
+}
+
+func toTimeString(_ period: Int, calendarSettings: CalendarSettings) -> String {
+	let startOfDay = getStartOfDay()
+	let interval = TimeInterval(period * calendarSettings.expensePeriod * 60)
+	let currentPeriod = startOfDay.advanced(by: interval)
+	let timeFormat = DateFormatter()
+	timeFormat.dateFormat = "HH:mm"
+	let timeString = timeFormat.string(from: currentPeriod)
+	return timeString
+}
+
+func getItemIndexOfCurrentTime(calendarSettings: CalendarSettings) -> Int {
+	let now = Date()
+	let startOfDay = getStartOfDay()
+	let difference = Int(startOfDay.distance(to: now))
+	let itemIndex = difference / 60 / calendarSettings.expensePeriod
+	return itemIndex
 }
 
 struct CalendarView_Previews: PreviewProvider {
