f8c5c90abb4bd38b908395e9f23d36f1c6497926
Jeremy Gurr <jeremy.gurr@gmail.com>
2020-08-01 08:51:04 -0700

    Fund recharge level feature: Add model changes

--- Time Budget/BackModel/TimeFund+CoreDataProperties.swift
+++ Time Budget/BackModel/TimeFund+CoreDataProperties.swift
@@ -2,14 +2,13 @@
 //  TimeFund+CoreDataProperties.swift
 //  Time Budget
 //
-//  Created by Jeremy Gurr on 7/21/20.
+//  Created by Jeremy Gurr on 8/1/20.
 //  Copyright © 2020 Pure Logic Enterprises. All rights reserved.
 //
 //
 
 import Foundation
 import CoreData
-
 
 extension TimeFund {
 
@@ -18,10 +17,29 @@ extension TimeFund {
     }
 
     @NSManaged public var balance: Float
+    @NSManaged public var frozen: Bool
     @NSManaged public var name: String
     @NSManaged public var order: Int16
-    @NSManaged public var frozen: Bool
+    @NSManaged public var recharge: Float
     @NSManaged public var budget: TimeBudget
+    @NSManaged public var expenses: NSSet?
     @NSManaged public var subBudget: TimeBudget?
+
+}
+
+// MARK: Generated accessors for expenses
+extension TimeFund {
+
+    @objc(addExpensesObject:)
+    @NSManaged public func addToExpenses(_ value: TimeExpense)
+
+    @objc(removeExpensesObject:)
+    @NSManaged public func removeFromExpenses(_ value: TimeExpense)
+
+    @objc(addExpenses:)
+    @NSManaged public func addToExpenses(_ values: NSSet)
+
+    @objc(removeExpenses:)
+    @NSManaged public func removeFromExpenses(_ values: NSSet)
 
 }
